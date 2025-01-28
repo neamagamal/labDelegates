0fd7bc5c9da26f8472d0581076d2a84f04de1213
neamagamal <neamagamal1213@gmail.com>
2025-01-28 16:00:46 +0200

    used tryparse instead of parse

--- Program.cs
+++ Program.cs
@@ -7,24 +7,18 @@ namespace lab6_del
     {
         static void Main(string[] args)
         {
-            int num1 = 0, num2 = 0, operation = 0;
+            int num1, num2, operation;
 
-            try
-            {
-                Console.WriteLine(" enter num1:");
-                num1 = int.Parse(Console.ReadLine());
-                Console.WriteLine(" enter num2 :");
-                num2 = int.Parse(Console.ReadLine());
-                Console.WriteLine(" Enter 1 for Addition");
-                Console.WriteLine(" Enter 2 for Subtraction");
-                Console.WriteLine(" Enter 3 for Multiplication");
-                Console.WriteLine(" Enter 4 for Division");
-                operation = int.Parse(Console.ReadLine());
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("invalid number try again");
-            }
+            Console.WriteLine("enter num1:");
+            int.TryParse(Console.ReadLine(), out num1);
+            Console.WriteLine("enter num2:");
+            int.TryParse(Console.ReadLine(), out num2);
+
+            Console.WriteLine("enter 1 for Addition");
+            Console.WriteLine("enter 2 for Subtraction");
+            Console.WriteLine("enter 3 for Multiplication");
+            Console.WriteLine("enter 4 for Division");
+            int.TryParse(Console.ReadLine(), out operation);
             if (operation == 1)
             {
                 calculatedDelegate(num1, num2, (a, b) => a + b);
